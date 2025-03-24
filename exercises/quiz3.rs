85c34d96b6ab6ccab629e01541ccaa90cfc150fc
gushuo <1156364963@qq.com>
2025-03-24 14:01:36 +0800

    q3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,16 +18,16 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<g:ToString> {
+    pub grade: g,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl <g:ToString>ReportCard<g>{
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.to_string())
     }
 }
 
@@ -52,7 +52,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
